0041e23bc0b9f81a97b0809eac59467478f3640d
ch-tseng <ch.tseng@sunplusit.com>
2018-07-04 16:01:35 +0800

    update

--- arduino/pointer/pointer.ino
+++ arduino/pointer/pointer.ino
@@ -4,6 +4,7 @@ SoftwareSerial Serial1(2, 3); // RX, TX
 byte pinNotify = 5;
 byte pinUp = 8;
 byte pinDown = 9;
+unsigned int delayTime = 500;
 
 boolean notifyState = 0;
 boolean upState = 0;
@@ -23,31 +24,24 @@ void loop() {
   upState = digitalRead(pinUp);
   downState = digitalRead(pinDown);
 
-  //Serial.print(upState);
-  //Serial.print(" / ");
-  //Serial.println(downState);
-
   if ( notifyState == 1) {
     Serial.println("Notice");
     Serial1.write('N');
-    delay(500);
+    delay(delayTime);
     Serial1.flush();
   }
 
   if (upState == 1) {
     Serial.println("Up");
     Serial1.write('U');
-    delay(500);
+    delay(delayTime);
     Serial1.flush();
   }
 
   if (downState == 1) {
     Serial.println("Down");
     Serial1.write('D');
-    delay(500);
+    delay(delayTime);
     Serial1.flush();
   }
-
-
-
 }
